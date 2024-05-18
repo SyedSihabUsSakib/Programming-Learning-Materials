0faeb5757717ce02bf28dbe90a04ab84c680537c
SyedSihabUsSakib <sakibshihab12@gmail.com>
2024-05-18 20:32:22 +0600

    M-4 D-2

---
+++ Data Structure/pointer.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define ff first
+#define ss second
+#define pb push_back
+#define endl '\n'
+#define w(t) ll test;cin>>test;while(test--)
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(0);
+#define iof  freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define pi acos(-1)
+int fx[]={0,0,1,-1,1,1,-1,-1};
+int fy[]={1,-1,0,0,-1,1,-1,1};
+const int mxn=1e9+7;
+
+
+int main()
+{
+    int a;
+    cin>>a;
+    int* ptr= &a;
+    int b = 5;
+    cout<<"The address of a is--->"<<&a<<"--->The address of ptr is---->"<<ptr<<endl;
+    ptr = &b;
+    cout<<&a<<endl;
+    cout<<a<<endl;
+    cout<<"The address of b is--->"<<&b<<endl;
+    cout<<"The address which is pointed to"<<endl;
+
+    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
+
+    *ptr = 200;
+    cout<<"The value of b is--->"<<b<<endl;
+    cout<<"The address of ptr is --->>>"<<ptr<<"--->The address of b is--->"<<&b<<endl;
+}
+
+
