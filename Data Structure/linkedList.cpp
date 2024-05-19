54e1950158b6f9ffd4462ea6112d10de84143c48
SyedSihabUsSakib <sakibshihab12@gmail.com>
2024-05-19 12:52:51 +0600

    M-4 D-3

---
+++ Data Structure/linkedList.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define ff first
+#define ss second
+#define pb push_back
+#define endl '\n'
+#define w(t) ll test;cin>>test;while(test--)
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(0);
+#define iof  freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define pi acos(-1)
+int fx[]={0,0,1,-1,1,1,-1,-1};
+int fy[]={1,-1,0,0,-1,1,-1,1};
+const int mxn=1e9+7;
+
+struct node{
+    int data;
+    struct node *link;
+};
+
+struct node *header = NULL;
+int main()
+{
+   struct node *element = new node();
+
+   element->data = 10;
+   element->link = NULL;
+   header = element;
+
+   struct node *ab = new node();
+   ab->data = 20;
+   ab->link = NULL;
+   element->link = ab;
+}
+
+
